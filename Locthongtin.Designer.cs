d7b4287ad6beeabf62a72a2712f0491b65da0c1f
Nhathao03 <yukunvip21@gmail.com>
2023-10-12 16:15:31 +0700

    up

--- Locthongtin.Designer.cs
+++ Locthongtin.Designer.cs
@@ -48,6 +48,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(218, 35);
             this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1235456";
             // 
             // comboBox1
             // 
